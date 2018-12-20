35398ad0c1e710d43030b55335597a7e1bbb903e
Monis Khan <mkhan@redhat.com>
2018-12-20 12:23:33 -0500

    wip005
    Signed-off-by: Monis Khan <mkhan@redhat.com>

--- pkg/operator2/oauth.go
+++ pkg/operator2/oauth.go
@@ -8,8 +8,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 
+	configv1 "github.com/openshift/api/config/v1"
 	kubecontrolplanev1 "github.com/openshift/api/kubecontrolplane/v1"
 	osinv1 "github.com/openshift/api/osin/v1"
+	"github.com/openshift/library-go/pkg/crypto"
 	"github.com/openshift/library-go/pkg/operator/resource/resourcemerge"
 )
 
@@ -31,6 +33,16 @@ func (c *osinOperator) handleOAuthConfig(configOverrides []byte) (*corev1.Config
 		accessTokenInactivityTimeoutSeconds = &timeout
 	}
 
+	var templates *osinv1.OAuthTemplates
+	emptyTemplates := configv1.OAuthTemplates{}
+	if oauthConfig.Spec.Templates != emptyTemplates {
+		templates = &osinv1.OAuthTemplates{
+			Login:             "", // TODO need to handle these secrets here and in deployments
+			ProviderSelection: "",
+			Error:             "",
+		}
+	}
+
 	identityProviders := make([]osinv1.IdentityProvider, 0, len(oauthConfig.Spec.IdentityProviders))
 	for _, idp := range oauthConfig.Spec.IdentityProviders {
 		identityProviders = append(identityProviders,
@@ -49,11 +61,44 @@ func (c *osinOperator) handleOAuthConfig(configOverrides []byte) (*corev1.Config
 
 	// TODO this pretends this is an OsinServerConfig
 	cliConfig := &kubecontrolplanev1.KubeAPIServerConfig{
+		GenericAPIServerConfig: configv1.GenericAPIServerConfig{
+			ServingInfo: configv1.HTTPServingInfo{
+				ServingInfo: configv1.ServingInfo{
+					BindAddress: "0.0.0.0:9443",
+					BindNetwork: "tcp4",
+					CertInfo: configv1.CertInfo{
+						CertFile: "", // needs to be signed by MasterCA from below
+						KeyFile:  "",
+					},
+					ClientCA:          "", // I think this can be left unset
+					NamedCertificates: nil,
+					MinTLSVersion:     crypto.TLSVersionToNameOrDie(crypto.DefaultTLSVersion()),
+					CipherSuites:      crypto.CipherSuitesToNamesOrDie(crypto.DefaultCiphers()),
+				},
+				MaxRequestsInFlight:   1000,   // TODO this is a made up number
+				RequestTimeoutSeconds: 5 * 60, // 5 minutes
+			},
+			CORSAllowedOrigins: nil,                    // TODO probably need this
+			AuditConfig:        configv1.AuditConfig{}, // TODO probably need this
+			KubeClientConfig: configv1.KubeClientConfig{
+				KubeConfig: "", // this should use in cluster config
+				ConnectionOverrides: configv1.ClientConnectionOverrides{
+					QPS:   400, // TODO figure out values
+					Burst: 400,
+				},
+			},
+		},
 		OAuthConfig: &osinv1.OAuthConfig{
-			MasterCA:                    nil, // TODO figure out if we need these to be set
-			MasterURL:                   "",
-			MasterPublicURL:             "",
-			AssetPublicURL:              "",
+			// TODO at the very least this needs to be set to self signed loopback CA for the token request endpoint
+			MasterCA: nil,
+			// TODO osin's code needs to be updated to properly use these values
+			// it should use MasterURL in almost all places except the token request endpoint
+			// which needs to direct the user to the real public URL (MasterPublicURL)
+			// that means we still need to get that value from the installer's config
+			// TODO ask installer team to make it easier to get that URL
+			MasterURL:                   "https://127.0.0.1:9443",
+			MasterPublicURL:             "https://127.0.0.1:9443",
+			AssetPublicURL:              "", // TODO do we need this?
 			AlwaysShowProviderSelection: false,
 			IdentityProviders:           identityProviders,
 			GrantConfig: osinv1.GrantConfig{
@@ -70,11 +115,7 @@ func (c *osinOperator) handleOAuthConfig(configOverrides []byte) (*corev1.Config
 				AccessTokenMaxAgeSeconds:            oauthConfig.Spec.TokenConfig.AccessTokenMaxAgeSeconds,
 				AccessTokenInactivityTimeoutSeconds: accessTokenInactivityTimeoutSeconds,
 			},
-			Templates: &osinv1.OAuthTemplates{
-				Login:             "", // TODO need to handle these secrets here and in deployments
-				ProviderSelection: "",
-				Error:             "",
-			},
+			Templates: templates,
 		},
 	}
 
